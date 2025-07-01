c68f9a68127c7a41be50b86c36580fac163d1d72
John Doe <node@gmail.com>
2025-07-01 13:49:11 +0300

    one more time

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/opdemonstraions/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +16,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/variables.scss" as *;`
+      }
+    }
+  }
 })
